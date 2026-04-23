9ebbc7b636ab97c7f8457f860a5844f3b11fdc62
Jeffrey Grober <129817632+JeffreyGrober@users.noreply.github.com>
2026-04-22 19:40:40 -0700

    Commit

--- controls.m
+++ controls.m
@@ -34,22 +34,57 @@ while 1
             %brick.MoveMotor('AB', 0);
         case 'leftarrow'
             disp ('left');
-            brick.MoveMotor('AD', 100);
-            brick.MoveMotor('CB', -100);
+            brick.MoveMotor('A', 100);
+            brick.MoveMotor('B', -100);
             %pause(0.1);
             %brick.MoveMotor('AD', 0);
         case 'rightarrow'
             disp('right');
-            brick.MoveMotor('AD', -100);
-            brick.MoveMotor('CB', 100);
+            brick.MoveMotor('A', -100);
+            brick.MoveMotor('B', 100);
             %pause(0.1);
             %brick.MoveMotor('CB', 0);
+        case 'k'
+            disp('k')
+            brick.MoveMotor('C', -100);
+        case 'l'
+            disp('l')
+            brick.MoveMotor('C', 100);
         case 'space'
             disp('space pressed');
-            brick.MoveMotor('ABCD', 0);
+            brick.MoveMotor('ABC', 0);
         case 0
             disp('no key pressed');
-            brick.MoveMotor('ABCD', 0);
+            brick.MoveMotor('AB', 0);
+        case 'p'
+            disp('auto mode')
+            distance = brick.UltrasonicDist(4);
+            display(distance);
+            brick.SetColorMode(3, 2);
+            color = brick.ColorCode(3);
+            display(color); 
+            while (color ~= 3)
+                brick.MoveMotor('AB', -50);
+                distance = brick.UltrasonicDist(4);
+                display(distance);
+                color = brick.ColorCode(3);
+                display(color);
+                touch = brick.TouchPressed(1);
+                touch2 = brick.TouchPressed(2);
+                if touch || touch2
+                    disp('touch')
+                    pause(0.5);
+                    brick.MoveMotor('AB', 50);
+                    pause(1);
+                    brick.MoveMotor('AB', 0);
+                    pause(0.5);
+                    brick.MoveMotor('A', 50);
+                    brick.MoveMotor('B', -50);
+                    pause(0.25);
+                    brick.MoveMotor('AB', 0);
+                    pause(0.5);
+                end
+            end
         case 'q'
             break;
     end
